39c0b997854a7e143011fda2619eaa92df9d25a6
hoinlee-moi <lhi3048@gmail.com>
2022-07-22 00:17:06 +0900

    modify & add : 회원가입 페이지 input 자동완성 제거 , 포스트 페이지 사진 틀 맞추기 , user페이지 완성
    signup.html에서 input태그 자동완성 부분 제거 , post.css 에서 포스트 페이지 사진 넘어가는 것 조정
    user페이지 프로필 업데이트 회원탈퇴까지 해서 기능구현 완료

--- js/user.js
+++ js/user.js
@@ -4,6 +4,7 @@ const token = $.cookie("mytoken")
 const paramArray = [];
 let profile_nickname ;
 let id = 0;
+let check_dup_result;
 
 const elementInfo = [{
     type : 'button',
@@ -26,7 +27,7 @@ const elementInfo = [{
 },{//프로필
     type : 'text,image',
     objs : {
-        nickmame : document.querySelector('.profile-container #name'),
+        nickname : document.querySelector('.profile-container #name'),
         introduce : document.querySelector('.profile-container #introduce'),
         profileImage : document.querySelector('.profile-container #profile-image')
     }
@@ -34,6 +35,8 @@ const elementInfo = [{
     type : 'input',
     objs : {
         nameUpdate : document.querySelector('.modal-window #input-nickname'),
+        password : document.querySelector('.modal-window #input-password'),
+        password2 : document.querySelector('.modal-window #input-password2'),
         addressUpdate : document.querySelector('.modal-window #input-address'),
         file : document.querySelector('#img-file-box #img'),
         imgfileName : document.querySelector('#img-file-box .file-name'),
@@ -45,12 +48,6 @@ const objs = elementInfo[0].objs
 const modalInfo = elementInfo[1].objs
 const profile = elementInfo[2].objs
 const updateProfile = elementInfo[3].objs
-//로그아웃
-function sign_out() {
-    alert('다음에 또 뵙겠습니다. ^^')
-    $.removeCookie('mytoken',{path:'/'})
-    window.location.href = "/"
-}
 
 //파라미터 값 가져오기
 function parameter(){
@@ -81,14 +78,14 @@ function img_up() {
             alert("jpg,jpeg,png 이미지만 사용 가능합니다.")
             const fileName = document.querySelector('#file-js-example .file-name');
             fileName.textContent = "";//스팬 값 초기화
-            $("#image_preview").toggleClass("is-hidden")
+            $(".modal-window #image_preview").css('display','none')
         } else {
-            file = $('#img').prop("files")[0];
+            file = updateProfile.file.files[0]
             blobURL = window.URL.createObjectURL(file);
-            $('#image_preview img').attr('src', blobURL);
-            $('#image_preview').slideDown(); //업로드한 이미지 미리보기
+            $('#image_preview #img_pre').attr('src', blobURL);
+            $('.modal-window #image_preview').slideDown(); //업로드한 이미지 미리보기
             $(this).slideUp(); //파일 양식 감춤
-            $("#image_preview").removeClass("is-hidden")
+            $(".modal-window #image_preview").css('display','block')
         }
     });
 }
@@ -220,7 +217,7 @@ function user_profile() {
 }
 //프로필데이터 넣기
 function profile_make(data) {
-    profile.nickmame.textContent = data['nickname']
+    profile.nickname.textContent = data['nickname']
     if(data['profileInfo'] === null) {
         profile.introduce.textContent = '아직 소개글이 없습니다.'
         profile.introduce.style.color = '#aaaaaa'
@@ -228,7 +225,7 @@ function profile_make(data) {
         profile.introduce.textContent = data['profileInfo']
     }
     if(data['profilePic'] === null) {
-        profile.profileImage.src = '/image/gogumaket.png'
+        profile.profileImage.src = '/image/gogumacat-profile.png'
     } else {
         profile.profileImage.src = data['profilePic']
     }
@@ -237,12 +234,21 @@ function profile_make(data) {
 function update_make(data) {
     updateProfile.nameUpdate.value = data['nickname']
     updateProfile.addressUpdate.value = data['address']
-    updateProfile.imgfileName.textContent = data['profilePic']
     updateProfile.introduce.value = data['profileInfo']
     if(data['profilePic'] != null){
     updateProfile.filePreview.src = data['profilePic']
+    updateProfile.imgfileName.textContent = data['profilePic']
+    } else {
+        updateProfile.filePreview.src = '/image/gogumacat-profile.png'
+        updateProfile.imgfileName.textContent = 'default'
     }
 }
+
+//카카오 초기 비밀번호 알려주기
+function kakao_pw_check(){
+    alert("카카오 로그인 시 초기 비밀번호는 카카오 이메일의 \"@\"앞 부분 입니다!\nEx) 이메일 : goguma@naver.com -> 비밀번호 : goguma")
+}
+
 //프로필 수정, 회원 탈퇴시 비밀번호 확인
 function check_pw(value){
     let pw_input = value.childNodes[3].childNodes[1]
@@ -264,7 +270,6 @@ function check_pw(value){
                   xhr.setRequestHeader("token", token);
             },
             success: function (result) {
-                console.log(result)
                 if(result) {
                     if(pw_input.id === 'input-check-pw'){
                         modalInfo.updatePw.style.display = 'none'
@@ -282,12 +287,12 @@ function check_pw(value){
     }
 }
 
-
+//비밀번호 형식확인
 function is_password(asValue) {
     var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
     return regExp.test(asValue);
 }
-
+//닉네임 중복 확인
 function check_dup_nick() {
     let nickname = updateProfile.nameUpdate.value
 
@@ -309,14 +314,17 @@ function check_dup_nick() {
             success: function (data) {
                 if (data >= 1) {
                     alert('이미 존재하는 닉네임입니다')
+                    check_dup_result = 0;
                 } else {
                     alert('사용할 수 있는 닉네임입니다')
+                    check_dup_result = 1;
                 }
+                return check_dup_result;
             }
+
         });
     }
 }
-
 //주소 검색
 function juso() {
     new daum.Postcode({
@@ -339,12 +347,113 @@ function juso() {
         }
     }).open();
 }
-
-//카카오 초기 비밀번호 알려주기
-function kakao_pw_check(){
-    alert("카카오 로그인 시 초기 비밀번호는 카카오 이메일의 \"@\"앞 부분 입니다!\nEx) 이메일 : goguma@naver.com -> 비밀번호 : goguma")
+function nickname_reset() {
+    check_dup_result = 0
 }
-
+function update_check() {
+    const old_nickname = profile.nickname.textContent
+    const nickname = updateProfile.nameUpdate.value
+    const password = updateProfile.password.value
+    const password2 = updateProfile.password2.value
+    const address = updateProfile.addressUpdate.value
+    const profilePic = updateProfile.file.files[0]
+    const profileInfo = updateProfile.introduce.value
+    if(nickname == "") {
+        updateProfile.nameUpdate.placeholder = '닉네임을 입력해주세요'
+        updateProfile.nameUpdate.focus()
+        return;
+    }  
+    if(nickname != old_nickname) {
+        if(check_dup_result == 0){
+            alert('닉네임 중복확인이 필요합니다')
+        }
+        return;
+    }
+    if(password == "") {
+        updateProfile.password.placeholder = '비밀번호를 입력해주세요'
+        updateProfile.password.focus()
+        return;
+    }
+    if(!is_password(password)) {
+        updateProfile.password.value = ""
+        updateProfile.password.placeholder = '비밀번호의 형식을 확인해주세요'
+        updateProfile.password.focus()
+        return;
+    } 
+    if(password2 == "") {
+        updateProfile.password2.placeholder = '비밀번호를 입력해주세요'
+        updateProfile.password2.focus()
+        return;
+    }
+    if(password2 != password) {
+        updateProfile.password2.value = ""
+        updateProfile.password2.placeholder = '비밀번호가 일치하지 않습니다'
+        updateProfile.password2.focus()
+        return;
+    }
+    if(address == "") {
+        updateProfile.addressUpdate.placeholder = '주소를 입력해주세요'
+        updateProfile.addressUpdate.focus()
+        return;
+    }
+    if(profileInfo =="") {
+        updateProfile.introduce.placeholder = '짧은 소개라도 입력해주세요'
+        updateProfile.introduce.focus()
+        return
+    }
+    let form_data = new FormData()
+    if (profilePic == undefined) {
+        form_data.append("nickname", nickname)
+        form_data.append("password", password2)
+        form_data.append("address", address)
+        form_data.append("profileInfo", profileInfo)
+    }
+    else {
+        form_data.append("nickname", nickname)
+        form_data.append("password", password2)
+        form_data.append("address", address)
+        form_data.append("profileInfo", profileInfo)
+        form_data.append("profilePic", profilePic)
+        }
+        console.log(form_data)
+        update(form_data)
+    }
+//업데이트 하기
+function update(form_data) {
+    $.ajax({
+        type: "PUT",
+        url: `${domain}/update_profile/${id}`,
+        data: form_data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        contentType : false,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("token", token);
+        },
+        enctype: "multipart/form-data",
+        success: function (response) {
+            alert('프로필 변경 완료되었습니다.')
+            window.location.replace(`/user.html?userId=${id}`)
+        }
+    })
+        
+}
+function userdelete() {
+    $.ajax({
+        type: "DELETE",
+        url: `${domain}/delete/${id}`,
+        data: {'id': id},
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("token", token);
+        },
+        success: function (response){
+            alert("이용해 주셔서 감사합니다.")
+            $.removeCookie('mytoken',{path:'/'})
+            window.location.href = "/login.html"
+        }
+    })
+}
 
 //로그아웃
 function sign_out() {
@@ -358,4 +467,5 @@ window.addEventListener('load',() => {
     parameter()
     get_write_posts(id)
     user_profile()
+    fileupload()
 })
