57c34d3214d1e2f1cb06359dab3186274e2d6262
hongggs <hongseoda@gmail.com>
2022-07-22 10:13:39 +0900

    feat: 거래 알림을 확인하고 확인을 하면 다시 안뜨게 체크하는 기능 추가

--- js/userInfo.js
+++ js/userInfo.js
@@ -13,8 +13,7 @@ function initUserInfo() {
                   userId = response["id"]
                   $("#loginName").text(`${response["nickname"]}님 환영합니다.`)
                   if(response["orders"]){
-                        console.log(response["orders"])
-                        $("#alarm-box").append(`<i id="alarm-btn" data-toggle="modal" data-target="#exampleModalScrollable" class="fa-solid fa-bell"></i>`)
+                        makeOrderContent(response["orders"])
                   }
             }
       })
@@ -31,10 +30,34 @@ function userPage() {
       }
 }
 
-function openOrderModal() {
-      $(".modal").fadeIn()
+function makeOrderContent(orders) {
+      console.log(orders)
+      //알림을 볼 수 있는 버튼 생성
+      $("#alarm-box").append(`<i id="alarm-btn" class="fa-solid fa-bell" data-toggle="modal" data-target="#order-modal"></i>`)
+      let tempHtml=""
+      for(let i =0; i < orders.length; i++) {
+            tempHtml += `<p style="margin: 20px;">
+                              <text onclick="location.href='/post.html?id=${orders[i]["post"]["postId"]}'">${orders[i]["post"]["sellerNickname"]}님과 거래 약속이 잡혔습니다!</text>
+                              <button onclick="checkOrder(${orders[i]["orderId"]})" style="float: right;">확인</button>
+                        </p> 
+                        <hr>`
+      }
+      $("#order-content").append(tempHtml)
 }
 
-function closeOrderModal() {
-      $(".modal").fadeOut()
+function checkOrder(orderId) {
+      let result = confirm("거래 알림을 확인하셨습니까?");
+      if(result) {
+            $.ajax({
+                  type: "POST",
+                  url: `${domain}/order/check/${orderId}`,
+                  data: {},
+                  beforeSend: function(xhr) {
+                        xhr.setRequestHeader("token", token);
+                  },
+                  success: function (response) {
+                        window.location.href = "/list.html"
+                  }
+            })
+      }
 }
